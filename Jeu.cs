fae3664c6406bceb2f8008bb21cf2ef877588b24
Arthur PINEAU <apineau3@la-joliverie.com>
2020-01-16 10:35:11 +0100

    p54

--- Jeu.cs
+++ Jeu.cs
@@ -12,6 +12,7 @@ namespace tp2048
 {
     public partial class Jeu : Form
     {
+        private int _mouvements = 0;
         public Jeu()
         {
             InitializeComponent();
@@ -21,15 +22,56 @@ namespace tp2048
         {
 
         }
-
-        private void TexteMouvement_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+        private void MessageEtat(string message)
+        {
+            LabelEtat.Text = message;
+        }
+        private void LabelEtat_Click(object sender, EventArgs e)
+        {
+            MessageEtat("Version 1.0");
+        }
+
+        private void NouveauJeu_Click(object sender, EventArgs e)
+        {
+            MessageEtat("Nouvelle partie");
+            LabelMouvement.Text = _mouvements.ToString();
+        }
+
+        private void Jeu_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageEtat($"Touche {Direction(e)}");
+            if(Direction(e)!= Sens.Autre)
+            {
+                _mouvements++;
+                LabelMouvement.Text = _mouvements.ToString();
+            }
+        }
+        public enum Sens
+        {
+            Haut, Bas, Droite, Gauche, Autre
+        }
+        private Sens Direction(KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Down: return Sens.Bas;
+                case Keys.Up: return Sens.Haut;
+                case Keys.Left: return Sens.Gauche;
+                case Keys.Right: return Sens.Droite;
+                default: return Sens.Autre;
+            }
+        }
+
+        private void Jeu_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = MessageBox.Show(
+                String.Format("Fermeture de l'application demandée pour {0}. Voulez-vous quitter ?",
+                e.CloseReason), "Fermeture...",
+                MessageBoxButtons.YesNo) == DialogResult.No;
         }
     }
 }
